f3d212956fd8dc9fc0bc1301f7dc9144b824c76b
Glitch (atahan-selfbot) <none>
2022-10-21 13:18:47 +0000

    ☕️☄️ Checkpoint ./komutlar/translate.js:26490528/2087:40747210/565 ./atahan.js:26490528/2 ./ayarlar.json:26490528/36

--- komutlar/translate.js
+++ komutlar/translate.js
@@ -1,10 +1,29 @@
-const translate = require('google-translate-api');
+const Discord = require("discord.js-selfbot-v13");
+const çeviri = require("google-translate-api")
+const db = require("quick.db");
+const ayarlar = require("../ayarlar.json");
+let basarili = ayarlar.basariliemoji;
+let basarisiz = ayarlar.basarisizemoji
+exports.run = async (client, message, args) => {
+  
+if (message.author.id === ayarlar.sahip) {
 
- if (cmd === "translate") {
-        let translateme = args.slice(0).join(" ")
-        translate(translateme, { to: config.translate }).then(res => {
-            msg.edit("", { embed: new Discord.RichEmbed().setTitle("Translate").setColor("#00C5FF").setDescription("From - ** " + res.from.language.iso + "**\nTo - ** " + config.translate + "**\nInput - **" + translateme + "**\nOutput :arrow_down:```" + res.text + "```").setFooter("Powered by Google") })
-        }).catch(err => {
-            msg.edit(":x: An error has occurred. Details: " + err)
-        });
-    }
+await message.delete()
+if (!args[0]) return
+if (!args.slice(1).join(' ')) return
+  
+message.channel.send(çeviri(args.slice(1).join(' '), {to: args[0]}))
+}};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["çeviri"],
+  permLevel: 4
+};
+
+exports.help = {
+  name: "trasnlate",
+  description: "Afk Olmanızı Sağlar.",
+  usage: "afk / afk <sebep>"
+};
