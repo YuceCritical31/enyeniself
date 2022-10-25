ac0eaf0155364ab941aff2012c3b2d284ba9783e
Glitch (atahan-selfbot) <none>
2022-10-25 13:38:25 +0000

    👌😂 Checkpoint ./komutlar/yazi-tura.js:26490528/1179:40747210/148

--- komutlar/yazi-tura.js
+++ komutlar/yazi-tura.js
@@ -7,23 +7,26 @@ exports.run = async(client, message, args) => {
   
 if (message.author.id === ayarlar.sahip) {
   
-  let cevaplar = ["yazı","tura"]
+  let cevaplar = ["Yazı","Tura"]
   let yt = cevaplar[Math.floor(Math.random() * cevaplar.length)]
 	
-await message.edit(`Para Çeviriliyor...`).then(x => {
-  
+await message.edit(`> Para Çeviriliyor...`).then(x => {
+setTimeout(() => {
+  message.edit("> Yazı...").then(x => setTimeout(() => {
+    x.edit("> Tura...")}, 1500))
+}, 1500)
 })
 }};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["yazıtura","cf","coinflip","yazı-tura","yazitura","yazi-tura"],
   permLevel: 4
 };
 
 exports.help = {
   name: "yt",
-  description: "Afk Olmanızı Sağlar.",
-  usage: "afk / afk <sebep>"
+  description: "Yazı Tura Atmanızı Sağlar.",
+  usage: "yt"
 };
